ed69ac1571af085479192032fb5fbe50c98e922c
Mandy Quan <mandy.quan@linkit.nl>
2022-12-03 20:58:26 +0100

    Added day3 part1

---
+++ day3/Program.cs
@@ -0,0 +1,42 @@
+﻿string[] input = File.ReadAllLines($"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}\\day3.txt");
+
+void Part1(string[] input)
+{
+    int total = 0;
+    int index = 0;
+
+    foreach (string line in input)
+    {
+        int halfLength = line.Length / 2;
+        List<char> firstHalf = new List<char>();
+        List<char> secondHalf = new List<char>();
+        char commonLetter = '\0';
+
+        for (int j=0; j<halfLength; j++) {
+            firstHalf.Add(line[j]);
+        }
+        for (int k=halfLength; k<line.Length; k++)
+        {
+            secondHalf.Add(line[k]);
+        }
+
+        for (int l = 0; l <= firstHalf.Count-1; l++)
+        {
+            char firstLetter = firstHalf[l];
+            for (int m = 0; m <= secondHalf.Count-1; m++)
+            {
+                if (firstLetter == secondHalf[m])
+                {
+                    commonLetter = secondHalf[m];
+                }
+            }
+        }
+
+        _ = char.IsLower(commonLetter) ? index = (int)commonLetter % 32 : index = ((int)commonLetter % 32) + 26;
+
+        total = total + index;
+    }
+    Console.WriteLine($"Total priority: {total}");
+}
+
+Part1(input);
