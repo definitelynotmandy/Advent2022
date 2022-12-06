3c38292802584edf679c8503e240b57fd363c849
Mandy Quan <mandy.quan@linkit.nl>
2022-12-06 20:15:48 +0100

    Added day3 part2

--- day3/Program.cs
+++ day3/Program.cs
@@ -36,7 +36,47 @@ void Part1(string[] input)
 
         total = total + index;
     }
-    Console.WriteLine($"Total priority: {total}");
+    Console.WriteLine($"Part 1: {total}");
+}
+
+void Part2(string[] input)
+{
+    List<List<string>> groups = GetGroups(input);
+    int total = 0;
+
+    foreach (List<string> group in groups)
+    {
+        var sack1 = group[0];
+        var sack2 = group[1];
+        var intersect = sack1.Intersect(sack2);
+        var common = intersect.Intersect(group[2]);
+        int index = 0;
+
+        foreach (char letter in common)
+        {
+            _ = char.IsLower(letter) ? index = index + (int)letter % 32 : index = index + ((int)letter % 32) + 26;
+        }
+
+        total = total + index;
+    }
+    Console.WriteLine($"Part 2: {total}");
+
+    List<List<string>> GetGroups(string[] input)
+    {
+        List<List<string>> groups = new();
+        List<string> current = new ();
+        foreach (string group in input)
+        {
+            current.Add(group);
+            if (current.Count == 3)
+            {
+                groups.Add(current);
+                current = new();
+            }
+        }
+        return groups;
+    }
 }
 
 Part1(input);
+Part2(input);
