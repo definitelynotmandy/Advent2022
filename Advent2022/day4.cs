72274db246d344acd0b87bc851ed7fe009419144
Mandy Quan <mandy.quan@hotmail.com>
2022-12-07 01:32:19 +0100

    clean up

--- Advent2022/day4.cs
+++ Advent2022/day4.cs
@@ -9,7 +9,6 @@
 
             var input = File.ReadAllLines(@$"{Environment.CurrentDirectory}\Inputs\day4.txt").Select(x => x.Split(",").Select(y => y.Split("-").ToList()).ToList()).ToList(); // [ [[A] [b]] [[C] [D]] ] a list with 2 lists, with each 2 lists, total of 4 in a pair of clowns: start1 start2, end1 end2
 
-
             foreach (var pair in input)
             {
                 _ = (((int.Parse(pair[1][0]) >= int.Parse(pair[0][0])) && (int.Parse(pair[1][1]) <= int.Parse(pair[0][1]))) || ((int.Parse(pair[1][0]) <= int.Parse(pair[0][0])) && (int.Parse(pair[1][1]) >= int.Parse(pair[0][1])))) ? overlap1++ : overlap1 + 0; //can you have a "do nothing if false" ternary?
@@ -22,9 +21,7 @@
             }
 
             Console.WriteLine($"\nDay 4\ta) {overlap1}\n\tb) {overlap2}");
-
         }
-
     }
 }
    
